eaf57afdfe8d77afddc15e334b46ee09c346e2f9
Arie <48937555+Rieko00@users.noreply.github.com>
2023-06-10 23:42:33 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function scrollToTarget(elementID) {
+  var element = document.getElementById(elementID);
+  element.scrollIntoView({
+    block: "start",
+    behavior: "smooth",
+  });
+}
+
+function scrollToTargetAdjusted(elementID, offset) {
+  var element = document.getElementById(elementID);
+  var headerOffset = offset;
+  var elementPosition = element.getBoundingClientRect().top;
+  var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: "smooth",
+  });
+}
